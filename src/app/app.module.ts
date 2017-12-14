c3385ae9c35abbb5fb4c6ae601ae57ff9486280f
Raghuvanshi <roraghuvanshi@deloitte.com>
2017-12-15 01:18:56 +0530

    css-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ export function highchartsFactory() {
     SidenavComponent,
     HealthreportComponent,
     PtoComponent,
-    DatepipePipe
+    DatepipePipe,
     HealthReportDynamicComponent
   ],
   imports: [
